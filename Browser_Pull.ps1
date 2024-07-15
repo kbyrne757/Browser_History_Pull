c30c5055bb8219e837bb16b466fd536b0e648024
Lemon Zest <55719650+kbyrne757@users.noreply.github.com>
2024-07-14 19:05:12 -0600

    added funciton to copy and move to desktop

--- Browser_Pull.ps1
+++ Browser_Pull.ps1
@@ -10,3 +10,26 @@ $zipFilePath = Join-Path -Path $desktopPath -ChildPath "BrowserHistoryfiles.zip"
 
 # Create a temporary directory to copy the history files
 $tempDir = New-Item -ItemType Directory -Path (Join-Path -Path $env:TEMP -ChildPath "BrowserHistoryTemp")
+
+# Function to copy history file & inform user if the file is not there
+function CopyFile {
+    param (
+        [string]$historyFilePath,
+        [string]$browserName
+    )
+    if (Test-Path -Path $historyFilePath) {
+        $destPath = Join-Path -Path $tempDir -ChildPath "$browserName-History"
+        Copy-Item -Path $historyFilePath -Destination $destPath -Force
+        Write-Host "$browserName copied."
+    } else {
+        Write-Host "$browserName history not found."
+    }
+}
+
+
+# Copy browser history files
+Copy-File -historyFilePath $chromeHistory -browserName "Chrome"
+Copy-File -historyFilePath $edgeHistory -browserName "Edge"
+
+# Compress the copied history files into a zip file
+Compress-Archive -Path $tempDir\* -DestinationPath $zipFilePath -Force
