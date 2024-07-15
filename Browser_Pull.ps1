a753fbe508aa798fd5ea92212754fcf1e30babd8
Lemon Zest <55719650+kbyrne757@users.noreply.github.com>
2024-07-14 18:56:37 -0600

    setup the zip and temp directory

--- Browser_Pull.ps1
+++ Browser_Pull.ps1
@@ -3,3 +3,10 @@
 # Create and set variables for common history files required 
 $chromeHistory = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\History"
 $edgeHistory = "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\History"
+
+# create variables for browser zip file
+$desktopPath = [Environment]::GetFolderPath('Desktop')
+$zipFilePath = Join-Path -Path $desktopPath -ChildPath "BrowserHistoryfiles.zip"
+
+# Create a temporary directory to copy the history files
+$tempDir = New-Item -ItemType Directory -Path (Join-Path -Path $env:TEMP -ChildPath "BrowserHistoryTemp")
