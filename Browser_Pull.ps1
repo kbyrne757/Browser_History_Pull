dc8267258be47cc8f7772a193037051ba2ea5fef
Lemon Zest <55719650+kbyrne757@users.noreply.github.com>
2024-07-14 18:55:02 -0600

    Initial creation
    Created file and set variables for the two common browser files

---
+++ Browser_Pull.ps1
@@ -0,0 +1,5 @@
+# Browser History Pull Script
+
+# Create and set variables for common history files required 
+$chromeHistory = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\History"
+$edgeHistory = "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\History"
